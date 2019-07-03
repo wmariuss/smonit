58b510a21c0c366d015f67219c88f58495c2a5d6
Marius Stanca <me@marius.xyz>
2019-07-03 13:58:30 +0300

    Manage service process with systemd

--- smonit/main.py
+++ smonit/main.py
@@ -1,22 +1,26 @@
 import os
 import falcon
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.executors.pool import ProcessPoolExecutor
 
 from smonit.views import Index
 from smonit.execution import Run
 from smonit.services.api import InfluxDb
 
 
-# influx = InfluxDb()
-# print(influx.list_databases)
-# print(influx.drop_database('smonit'))
-# print(influx.create_database('smonit'))
-# pprint.pprint(influx.query('select * from highstate_disabled'))
-
 run = Run()
 interval = os.environ.get('SCHEDULER_INTERVAL', 60)
 
-scheduler = BackgroundScheduler()
+executors = {
+        'default': {'type': 'threadpool', 'max_workers': 10},
+        'processpool': ProcessPoolExecutor(max_workers=3)
+    }
+job_defaults = {
+        'coalesce': False,
+        'max_instances': 3
+    }
+
+scheduler = BackgroundScheduler(executors=executors, job_defaults=job_defaults)
 scheduler.add_job(run.jobs_global, 'interval', minutes=2, id='global')
 scheduler.add_job(run.jobs_minion, 'interval', minutes=int(interval), id='minion')
 scheduler.start()
--- start.sh
+++ start.sh
@@ -8,8 +8,8 @@ if [ $# -eq 0 ]; then
     echo "Please give an argument to continue the script. Eg. dev or prod."
 else
     if [ "$STAGE" == "dev" ]; then
-        gunicorn -n smonit smonit.main:app --reload -b 0.0.0.0:8000 --timeout 90 --log-level info
+        gunicorn -n smonit smonit.main:app --reload -b 0.0.0.0:8000 --timeout 90 --log-level DEBUG
     elif [ "$STAGE" == "prod" ]; then
-        gunicorn -n smonit smonit.main:app -b 0.0.0.0:8000 --timeout 90 --log-level info
+        gunicorn -n smonit smonit.main:app -b 0.0.0.0:8000 --pid /run/smonit/smonit.pid --timeout 90 --log-level INFO
     fi
 fi
